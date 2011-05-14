6e0a654a4259d391f584de92c5be098f5257b866
Karl Seguin <karlseguin@gmail.com>
2011-05-14 16:36:18 +0800

    handle not having a hoptoad key'

--- lib/settings.rb
+++ lib/settings.rb
@@ -5,6 +5,10 @@ class Settings
     raise MissingConfigOptionError, "#{key.to_s} is not in the config file" unless @@settings.include?(key.to_s)
     @@settings[key.to_s]
   end
+  
+  def self.hoptoad_key
+    @@settings['hoptoad_key']
+  end
 
   class MissingConfigOptionError < StandardError;
   end
