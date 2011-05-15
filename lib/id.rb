972815da2826dcba1f074e16c8e591ef953e110e
Karl Seguin <karlseguin@gmail.com>
2011-05-15 19:58:22 +0800

    secret can no longer contain "

--- lib/id.rb
+++ lib/id.rb
@@ -16,6 +16,6 @@ module Id
     ((Time.now - id.generation_time) / 86400).floor > days
   end
   def self.secret
-    (0..(rand(15)+20)).map{48.+(rand(74)).chr}.join
+    (0..(rand(15)+20)).map{48.+(rand(74)).chr}.join.gsub('\\', '[]')
   end
 end
