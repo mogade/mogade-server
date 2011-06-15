ae9b1d0a75b46ba97bbba2917a17e0143f7e94c8
Karl Seguin <karlseguin@gmail.com>
2011-06-15 22:16:48 +0800

    read images directly into memory across all web servers

--- lib/file_storage.rb
+++ lib/file_storage.rb
@@ -4,13 +4,14 @@ class FileStorage
   def self.save_image(filename, stream, previous, generate_thumbnail)
     FileStorage.delete_image(previous) unless previous.nil?
     name = Id.new.to_s + '_' + filename
-    FileStorage.save_thumbnail(name, stream) if generate_thumbnail
-    FileStorage.save_to_aws(name, stream, {'Cache-Control' => 'public,max-age=31536000'})
+    buffer = FileStorage.stream_to_buffer(stream)
+    FileStorage.save_thumbnail(name, buffer) if generate_thumbnail
+    FileStorage.save_to_aws(name, buffer, {'Cache-Control' => 'public,max-age=31536000'})
     name
   end
   
-  def self.save_thumbnail(name, stream)
-    ImageScience.with_image_from_memory(stream.read) do |image|
+  def self.save_thumbnail(name, buffer)
+    ImageScience.with_image_from_memory(buffer.string) do |image|
       image.fit_within(100, 150) do |thumb|
         FileStorage.save_to_aws('thumb' + name, thumb.buffer(File.extname(name)), {'Cache-Control' => 'public,max-age=31536000'})
       end
@@ -23,7 +24,14 @@ class FileStorage
   end
   
   private
-  def self.save_to_aws(name, data, options = {})
-    AWS::S3::S3Object.store(name, data, Settings.aws['bucket'], options)
+  def self.save_to_aws(name, buffer, options = {})
+    AWS::S3::S3Object.store(name, buffer, Settings.aws['bucket'], options)
+  end
+  def self.stream_to_buffer(stream) #solves performance inconsistencies across different servers
+    return stream if stream.is_a?(StringIO) 
+    #protect us a bit against something odd and bad happening
+    #todo make this configurable or something
+    return nil if stream.size > 250 * 1024  
+    StringIO.new(stream.read.to_s)
   end
 end
