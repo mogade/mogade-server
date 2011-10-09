14f8678aae7dc3c7377a5cf4afa4149638b985e0
Karl Seguin <karlseguin@gmail.com>
2011-10-09 17:05:18 +0800

    custom stats report

---
+++ spec/models/stat/load_custom_data_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe Stat, :load_custom_data do
+  before(:each) do
+    @start = Time.utc(2012, 5, 10)
+    date = @start
+    5.times do |i|
+      Store.redis.incrby("s:custom:game_id:1:#{date.strftime("%y%m%d")}", i + 1000)
+      Store.redis.incrby("s:custom:game_id:2:#{date.strftime("%y%m%d")}", i + 100)
+      Store.redis.incrby("s:custom:game_id:3:#{date.strftime("%y%m%d")}", i)
+      Store.redis.incrby("s:custom:game_id:4:#{date.strftime("%y%m%d")}", i + 10000)
+      Store.redis.incrby("s:custom:game_id:5:#{date.strftime("%y%m%d")}", i + 100000)
+      date += 86400
+    end
+    @game = FactoryGirl.build(:game, {:id => 'game_id'})
+  end
+  
+  it "returns the custom stats for the specified date" do
+    stats = Stat.load_custom_data(@game, @start, @start + 4 * 86400)
+    stats[:data].should == [["1000", "1001", "1002", "1003", "1004"], ["100", "101", "102", "103", "104"], ["0", "1", "2", "3", "4"], ["10000", "10001", "10002", "10003", "10004"], ["100000", "100001", "100002", "100003", "100004"]]
+  end
+  
+end
