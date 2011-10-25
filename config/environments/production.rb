70c58598fa5a2cdb28061f3264e00d9622259a1f
Karl Seguin <karlseguin@gmail.com>
2011-10-25 14:46:38 +0800

    changed asset_host variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Mogade::Application.configure do
   config.assets.compile = false
   config.assets.digest = true
   config.log_level = Logger::ERROR
-  ActionController::Base.asset_host = Proc.new { |source, request|
+  config.action_controller.asset_host = Proc.new { |source, request|
     request.ssl? ? Settings.ssl_cdn_url : Settings.cdn_url
   }
 end
