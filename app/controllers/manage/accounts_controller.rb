80847908dc46ddac450e7bd2fa9fe766cb4c10e9
Karl Seguin <karl@openmymind.net>
2012-06-15 09:04:14 +0800

    fixed some common 404s

--- app/controllers/manage/accounts_controller.rb
+++ app/controllers/manage/accounts_controller.rb
@@ -1,18 +1,22 @@
 class Manage::AccountsController < Manage::ManageController
   skip_before_filter :ensure_not_https
   before_filter :ensure_https
-    
+
   def new
     @signup = Signup.new
   end
-  
+
+  def register
+    redirect_to :action => :new
+  end
+
   def create
     @signup = Signup.new(params)
     begin
       developer = @signup.to_developer
       if !developer.nil? && developer.save!
         url = url_for :action => 'activate', :key => developer.action, :only_path => false
-        Notifier.welcome(developer, url) 
+        Notifier.welcome(developer, url)
         return
       end
     rescue Mongo::OperationFailure
@@ -20,33 +24,33 @@ class Manage::AccountsController < Manage::ManageController
     end
     render :action => 'new'
   end
-  
+
   def activate
     id = Id.from_string(params[:key])
-    return if id.nil? || Id.expired?(id, 2) 
-    
+    return if id.nil? || Id.expired?(id, 2)
+
     developer = Developer.find_by_action(id)
     return if developer.nil?
     return signin(developer.activate!)
   end
-  
+
   def forgot
     render :layout => 'manage_single'
   end
 
   def send_reset
-    developer = Developer.set_new_action(params[:email])   
-    if developer.nil? 
-      set_error('the email could not be found') 
+    developer = Developer.set_new_action(params[:email])
+    if developer.nil?
+      set_error('the email could not be found')
       render :action => :forgot
       return
     end
-    
+
     url = url_for :action => 'reset', :key => developer.action, :only_path => false
     Notifier.reset_password(developer, url)
     render :layout => 'manage_single'
   end
-  
+
   def reset
     @key = params[:key]
     render :layout => 'manage_single'
@@ -55,7 +59,7 @@ class Manage::AccountsController < Manage::ManageController
   def reseted
     key = Id.from_string(params[:key])
     @key = key.to_s unless key.nil?
-    if key.nil? || Id.expired?(key, 1) 
+    if key.nil? || Id.expired?(key, 1)
       set_error('there was a problem loading your account, please try again (1)')
       render :reset and return
     end
@@ -64,7 +68,7 @@ class Manage::AccountsController < Manage::ManageController
       set_error('there was a problem loading your account, please try again (2)')
       render :reset and return
     end
-    if developer.reset_password(params[:password]) 
+    if developer.reset_password(params[:password])
       return signin(developer)
     end
     render :reset
--- app/controllers/manage/manage_controller.rb
+++ app/controllers/manage/manage_controller.rb
@@ -1,43 +1,47 @@
 class Manage::ManageController < ApplicationController
   layout 'manage_dual'
-  
+
   def faq
     render :layout => 'manage_single'
   end
-  
+
   def tos
   end
-  
+
   def drivers
     render :layout => 'manage_single'
   end
-  
+
   def api
   end
-  
-  private 
+
+  def libraries
+    redirect_to :action => :api
+  end
+
+  private
   def signin(developer)
     session[:dev_id] = developer.id.to_s
     redirect_to '/manage/games/'
   end
-  
+
   def set_error(msg, now = true)
     now ? flash.now[:error] = msg : flash[:error] = msg
   end
-  
+
   def set_info(msg, now = true)
     now ? flash.now[:info] = msg : flash[:info] = msg
   end
-  
+
   def ensure_logged_in
     load_developer
     redirect_to url_for :controller => 'sessions', :action => 'new' if @current_developer.nil?
   end
-  
+
   def ensure_leaderboard
     id = params[:id]
     return handle_access_denied unless Id.valid? id
-    
+
     @leaderboard = Leaderboard.find_by_id(Id.from_string(id))
     return handle_access_denied if @leaderboard.nil? || @leaderboard.game_id != @game.id
     true
@@ -52,12 +56,12 @@ class Manage::ManageController < ApplicationController
   def load_game
     id = params.include?(:game_id) ? params[:game_id] : params[:id]
     return handle_access_denied unless Id.valid? id
-  
+
     @game = Game.find_by_id(Id.from_string(id))
     return handle_access_denied if @game.nil?
   end
-  
-  def handle_access_denied    
+
+  def handle_access_denied
     @game = nil
     set_error('you do not have access to perform that action', false)
     redirect_to url_for :controller => 'games', :action => 'index'
