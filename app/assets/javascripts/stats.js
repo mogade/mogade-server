7e9c196659278e4d4808446f5d0b42847c225b89
Karl Seguin <karl@openmymind.net>
2012-06-23 20:35:19 -0400

    calendar is driven by UTC date rather than user data

--- app/assets/javascripts/stats.js
+++ app/assets/javascripts/stats.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     if ($customSelect.data['expanded'] === true) { hideCustomSelect(); loadData(); }
     else {
       $customSelect.css({height: 'auto'})
-      $customSelect.data['expanded'] = true;  
+      $customSelect.data['expanded'] = true;
     }
   });
 
@@ -24,7 +24,7 @@ $(document).ready(function() {
 
   function hideCustomSelect() {
     $customSelect.css({height: '28px'})
-    $customSelect.data.expanded = false;  
+    $customSelect.data.expanded = false;
   }
 
   $customSelect.children('div').not(':first').click(function(e) {
@@ -36,7 +36,7 @@ $(document).ready(function() {
       e.stopPropagation();
       return true;
     }
-    
+
     if ($input.is(':checked')) {
       $input.attr('checked', null);
     } else if (selectCustomInputs().length < 5) {
@@ -51,7 +51,8 @@ $(document).ready(function() {
 
   var $graph = $('#graph');
   var $raw = $('#raw tbody');
-  var now = new Date();
+  var x = new Date();
+  var now = new Date(x.getUTCFullYear(), x.getUTCMonth(), x.getUTCDate(),  x.getUTCHours(), x.getUTCMinutes(), x.getUTCSeconds());
   var lastWeek = new Date();
   lastWeek.setDate(now.getDate() - 7);
   $('#date').dateRange({
@@ -60,21 +61,21 @@ $(document).ready(function() {
     maximumDate: now,
     selected: loadData
   });
-  
+
   var numberOfSeries = parseInt($('#custom').val()) == 1 ? 5 : 3;
-  
+
   var graphOptions = {
     xaxis: { mode: 'time', timeformat: '%d %b', minTickSize: [1, 'day']  , monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'] },
     legend: {backgroundOpacity: 0, container: $('#legend'), noColumns: numberOfSeries},
     grid: {hoverable: true}
   };
-  
+
   function loadData() {
     var range = $('#date').dateRange({command: 'getRange'});
     var from = Date.UTC(range[0].getFullYear(), range[0].getMonth(), range[0].getDate());
     var to = Date.UTC(range[1].getFullYear(), range[1].getMonth(), range[1].getDate());
     var data = {id: $('#id').val(), from: Math.round(from/1000), to: Math.round(to/1000), authenticity_token: AUTH_TOKEN};
-    if (parseInt($('#custom').val()) == 1) { 
+    if (parseInt($('#custom').val()) == 1) {
       data['custom'] = true;
       data['custom_ids'] = [];
       selectCustomInputs().each(function() {
@@ -83,9 +84,9 @@ $(document).ready(function() {
     }
     $.post('/manage/stats/data', data, responseReceived, 'json');
   }
-  
+
   function responseReceived(r) {
-    if (parseInt($('#custom').val()) == 1) { 
+    if (parseInt($('#custom').val()) == 1) {
       var $ths = $('#raw thead tr th').filter(':not(:first)').text('');
       selectCustomInputs().each(function(i) {
         $ths.eq(i).text($(this).siblings('span').text())
@@ -101,7 +102,7 @@ $(document).ready(function() {
        series[i].push([new Date(from.getTime() + (j * 86400000)), value]);
       }
     }
-    
+
     var $headers = $raw.siblings('thead').find('th');
     var graphData = []
     for (var i = 0; i < numberOfSeries; ++i) {
@@ -134,14 +135,13 @@ $(document).ready(function() {
     if (existing.s == meta.s && existing.d == meta.d) { return; }
     $tip.text(item.datapoint[1]).show().css({top: item.pageY-20, left: item.pageX+10}).data('meta', meta);
   });
-  
-  
+
+
   $('<iframe>').attr('id', 'downloadFrame').css('display', 'none').appendTo($('body'));
   //Download stuff
-  $('#downloadMenu a').click(function() { 
+  $('#downloadMenu a').click(function() {
     var frame = $('#downloadFrame')[0];
     frame.src = '/manage/stats/data?id=' + $('#id').val() + '&year=' + $(this).attr('data-year');
     return false;
   });
 });
-  
